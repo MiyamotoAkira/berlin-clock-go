179473873e8e1cef05966f7f89dbf1f03824f259
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2024-08-18 20:16:40 +0100

    Check for evenness on seconds

--- berlin.go
+++ berlin.go
@@ -1,7 +1,29 @@
 package berlin_clock
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func GetClockString(time string) string {
-	return `O
+	second := time[len(time)-1 : len(time)]
+
+	converted, err := strconv.Atoi(second)
+
+	if err != nil {
+		fmt.Print("error")
+	}
+
+	is_even := converted%2 == 0
+
+	if is_even {
+		return `O
+OOOO
+OOOO
+OOOOOOOOOOO
+OOOO`
+	}
+	return `Y
 OOOO
 OOOO
 OOOOOOOOOOO
--- berlin_test.go
+++ berlin_test.go
@@ -20,3 +20,51 @@ OOOO`
 	}
 
 }
+
+func TestOneSecondPastMidnight(t *testing.T) {
+
+	actual := berlin.GetClockString("00:00:01")
+
+	expected := `Y
+OOOO
+OOOO
+OOOOOOOOOOO
+OOOO`
+
+	if actual != expected {
+		t.Errorf("Wrong clock. Expected\n%s\nbut got\n%s\ninstead", expected, actual)
+	}
+
+}
+
+func TestTwoSecondsPastMidnight(t *testing.T) {
+
+	actual := berlin.GetClockString("00:00:02")
+
+	expected := `O
+OOOO
+OOOO
+OOOOOOOOOOO
+OOOO`
+
+	if actual != expected {
+		t.Errorf("Wrong clock. Expected\n%s\nbut got\n%s\ninstead", expected, actual)
+	}
+
+}
+
+func TestThreeSecondsPastMidnight(t *testing.T) {
+
+	actual := berlin.GetClockString("00:00:03")
+
+	expected := `Y
+OOOO
+OOOO
+OOOOOOOOOOO
+OOOO`
+
+	if actual != expected {
+		t.Errorf("Wrong clock. Expected\n%s\nbut got\n%s\ninstead", expected, actual)
+	}
+
+}
